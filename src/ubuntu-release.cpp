b35011303136addea915a473a05786578c34b6c0
Anadi Sharma <asharma@uptycs.com>
2024-10-08 12:44:40 -0700

    Include Rapidjson

--- src/ubuntu-release.cpp
+++ src/ubuntu-release.cpp
@@ -3,10 +3,19 @@
 
 using namespace Multipass;
 
+const std::string kUbuntuReleasesURL = "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json";;
+
 int main()
 {
     CurlHTTPRequest req;
-    std::string sRes = req.get("https://www.google.com");
-    printf("\n%s\n", sRes.c_str());
+    std::string sRes = req.get(kUbuntuReleasesURL);
+    rapidjson::Document doc;
+    doc.Parse(sRes.c_str());
+    if(doc.HasParseError())
+    {
+        std::cerr << "Error parsing JSON" << std::endl;
+        return 1;
+    }
+    const rapidjson::Value &releases = doc["products"]["com.ubuntu.cloud:released:download"]["versions"];
     return 0;
 }
